c2c4da1b78f24637c3531b1bcb2901e3a3893a66
Jay Bharatkumar Soni <jb2soni@uwaterloo.ca>
2022-02-21 22:34:36 +0000

    Update LoginDAO.py removed unnecessary print statements. Change: userId to email

--- Catch_Up/project/com/dao/LoginDAO.py
+++ Catch_Up/project/com/dao/LoginDAO.py
@@ -4,19 +4,18 @@ from project.com.vo.LoginVO import LoginVO
 
 class LoginDAO:
 	def insertLogin(self, loginVO):
-		print('inside LoginDAO')
 		db.session.add(loginVO)
 		db.session.commit()
 
 	def validateLogin(self, loginVO):
-		loginList = LoginVO.query.filter_by(userId = loginVO.userId, password = loginVO.password)
+		loginList = LoginVO.query.filter_by(email = loginVO.email, password = loginVO.password)
 		return loginList
 
 	def validateUser(self, loginVO):
-		loginList = LoginVO.query.filter_by(userId = loginVO.userId)
+		loginList = LoginVO.query.filter_by(email = loginVO.email)
 		return loginList
 
 	def updatePassword(self, loginVO):
-		loginList = LoginVO.query.filter_by(userId = loginVO.userId).first()
+		loginList = LoginVO.query.filter_by(email = loginVO.email).first()
 		loginList.password = loginVO.password
 		db.session.commit()
