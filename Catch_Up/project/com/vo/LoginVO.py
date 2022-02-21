503186b2567e935659ed83fb43dbab4fb391788b
Jay Bharatkumar Soni <jb2soni@uwaterloo.ca>
2022-02-21 22:35:25 +0000

    Update LoginVO.py Added firstname, lastname, gender, category field. change: userId to email.

--- Catch_Up/project/com/vo/LoginVO.py
+++ Catch_Up/project/com/vo/LoginVO.py
@@ -3,14 +3,22 @@ from project import db
 class LoginVO(db.Model):
 	__tablename__ = "loginmaster"
 	Id = db.Column('Id', db.Integer, primary_key = True, autoincrement = True)
-	userId = db.Column('userId', db.String(100))
+	firstname = db.Column('firstname', db.String(100))
+	lastname = db.Column('lastname', db.String(100))
+	email = db.Column('email', db.String(100))
 	password = db.Column('password', db.String(100))
+	gender = db.Column('gender', db.String(100))
+	category = db.Column('category', db.String(100))
 
 	def as_dict(self):
 		return{
 			'Id': self.Id,
-			'userId': self.userId,
-			'password': self.password
+			'firstname': self.firstname,
+			'lastname': self.lastname,
+			'email': self.email,
+			'password': self.password,
+			'gender': self.gender,
+			'category': self.category
 		}
 
 db.create_all()
