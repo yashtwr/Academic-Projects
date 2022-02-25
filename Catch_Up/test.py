567d9ee9b335d468d3fc7a6cc6ebd70547accb4b
Dhruv Jayesh Dholakia <djdholakia@uwaterloo.ca>
2022-02-25 06:31:23 +0000

    This update test.py should give 91% coverage

--- Catch_Up/test.py
+++ Catch_Up/test.py
@@ -2,11 +2,13 @@ import email
 from project import app
 import unittest
 
+
+
 class FlaskTestCases(unittest.TestCase):
     def test_signup(self):
         tester = app.test_client(self)
         response = tester.get('/signup', content_type='html/text')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
     def test_signup_loads(self):
         tester = app.test_client(self)
@@ -15,20 +17,27 @@ class FlaskTestCases(unittest.TestCase):
 
     def test_correct_signup(self):
         tester = app.test_client(self)
-        tester.get('/signup',content_type='html/text')
-        response = tester.post('/insertUser',data=dict(firstname="admin", lastname="tester", email="admin@uwaterloo.ca", password="Abcd1234", confirmpassword="Abcd1234", gender="female", category="student"), follow_redirects=True)
-        self.assertTrue(response.request.path, "/login")
+        tester.get('/signup', content_type='html/text')
+        response = tester.post('/insertUser',
+                               data=dict(firstname="admin", lastname="tester", email="admin@uwaterloo00.ca",
+                                         password="Abcd1234", confirmpassword="Abcd1234", gender="female",
+                                         category="student"), follow_redirects=True)
+        
+        self.assertTrue(response, "/login")
 
     def test_incorrect_signup(self):
         tester = app.test_client(self)
-        tester.get('/signup',content_type='html/text')
-        response = tester.post('/insertUser',data=dict(firstname="admin", lastname="tester", email="admin@uwaterloo.ca", password="Abcd1234", confirmpassword="Abcd1234", gender="female", category="student"), follow_redirects=True)
+        tester.get('/signup', content_type='html/text')
+        response = tester.post('/insertUser',
+                               data=dict(firstname="admin", lastname="tester", email="admin@uwaterloo00.ca",
+                                         password="Abcd1234", confirmpassword="Abcd1234", gender="female",
+                                         category="student"), follow_redirects=True)
         self.assertIn(b'User already exists!', response.data)
 
     def test_login(self):
         tester = app.test_client(self)
         response = tester.get('/login', content_type='html/text')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
     def test_login_loads(self):
         tester = app.test_client(self)
@@ -37,24 +46,39 @@ class FlaskTestCases(unittest.TestCase):
 
     def test_correct_login(self):
         tester = app.test_client(self)
-        response = tester.post('/login',data=dict(email="admin@uwaterloo.ca", password="Abcd1234"), follow_redirects=True)
-        self.assertIn(b'Welcome admin@uwaterloo.ca!', response.data)
+        response = tester.post('/login', data=dict(email="admin@uwaterloo00.ca", password="Abcd1234"),
+                               follow_redirects=True)
+        self.assertIn(b'Welcome admin@uwaterloo00.ca!', response.data)
 
     def test_incorrect_login(self):
         tester = app.test_client(self)
-        response = tester.post('/login',data=dict(email="tester@uwaterloo.ca", password="Abcd1234"), follow_redirects=True)
+        response = tester.post('/login', data=dict(email="tester@uwaterloo00.ca", password="Abcd1234"),
+                               follow_redirects=True)
         self.assertIn(b'Login Failed!', response.data)
 
     def test_forgotPassword(self):
         tester = app.test_client(self)
         response = tester.get('/forgotPassword', content_type='html/text')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
     def test_forgotPassword_loads(self):
         tester = app.test_client(self)
         response = tester.get('/forgotPassword', content_type='html/text')
         self.assertTrue(b'Save' in response.data)
 
+    def test_forgotPassword_change_password(self):
+        tester = app.test_client(self)
+        response = tester.post('/updatePassword', data=dict(email="admin@uwaterloo00.ca", confirmpswd="Abcd1234"),
+                               follow_redirects=True)
+        
+        print('***********', response.data)
+        self.assertTrue(response, "/login")
+
+    def test_forgotPassword_no_user_exist(self):
+        tester = app.test_client(self)
+        response = tester.post('/updatePassword', data=dict(email="admin@uwaterloo009.ca", confirmpswd="Abcd1234"),
+                               follow_redirects=True)
+        self.assertIn(b'please enter valid user name',response.data)
 
 if __name__ == '__main__':
     unittest.main()
