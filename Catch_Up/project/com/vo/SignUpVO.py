37c8db4ae6eb52c8ea84ff66a9346f1f02927f3f
Dhruv Jayesh Dholakia <djdholakia@uwaterloo.ca>
2022-02-25 01:42:16 +0000

    Update SignUpVO.py

--- Catch_Up/project/com/vo/SignUpVO.py
+++ Catch_Up/project/com/vo/SignUpVO.py
@@ -5,20 +5,18 @@ class SignUpVO(db.Model):
 	Id = db.Column('Id', db.Integer, primary_key = True, autoincrement = True)
 	firstname = db.Column('firstname', db.String(30), nullable = False)
 	lastname = db.Column('lastname', db.String(30), nullable = False)
-	email = db.Column('email', db.String(length=100), nullable = False, unique=True)
-	password = db.Column('password', db.String(length=60),nullable = False)
 	gender = db.Column('gender', db.String(20), nullable = False)
 	category = db.Column('category', db.String(20), nullable = False)
+	signup_LoginId = db.Column('signup_LoginId', db.Integer, db.ForeignKey(LoginVO.loginId))
 
 	def as_dict(self):
 		return{
 			'Id': self.Id,
 			'firstname': self.firstname,
 			'lastname': self.lastname,
-			'email': self.email,
-			'password': self.password,
 			'gender': self.gender,
-			'category': self.category
+			'category': self.category,
+			'signup_loginId': self.signup_LoginId
 		}
 
 db.create_all()
