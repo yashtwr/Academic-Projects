79c538fc9650cfcf7d1fa61b58cffbcadec83e05
Priyanka Jain <p52jain@uwaterloo.ca>
2022-03-01 10:47:02 +0530

    change in contoller/__init__.py

--- Catch_Up/project/com/controller/__init__.py
+++ Catch_Up/project/com/controller/__init__.py
@@ -2,3 +2,4 @@
 # import project.com.controller.routes
 import project.com.controller.LoginController
 import project.com.controller.SignupController
+import project.com.controller.ProfileController
