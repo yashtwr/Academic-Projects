1d2d4d1ad913d1b54149f1a10034967aeb47aacf
Priyanka Jain <p52jain@uwaterloo.ca>
2022-03-07 23:11:36 +0530

    HobbiesVO.py

---
+++ Catch_Up/project/com/vo/HobbiesVO.py
@@ -0,0 +1,16 @@
+from project import db
+from project.com.vo.LoginVO import LoginVO
+
+class HobbiesVO(db.Model):
+    __tablename__ = "hobbiesmaster"
+    Id = db.Column('Id', db.Integer, primary_key=True, autoincrement=True)
+    hobbies = db.Column('hobbies',db.String(20))
+    hobbies_loginId = db.Column('hobbies_loginId', db.Integer, db.ForeignKey(LoginVO.loginId))
+
+    def as_dict(self):
+        return {
+            'Id': self.Id,
+            'hobbies': self.hobbies
+        }
+
+db.create_all()
