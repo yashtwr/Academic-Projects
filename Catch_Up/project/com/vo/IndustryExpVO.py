f852984f3724544c86bc5c93e224b49a5f892f9e
Priyanka Jain <p52jain@uwaterloo.ca>
2022-03-11 02:01:30 +0530

    updated IndustryExpVO.py

--- Catch_Up/project/com/vo/IndustryExpVO.py
+++ Catch_Up/project/com/vo/IndustryExpVO.py
@@ -4,11 +4,10 @@ from project.com.vo.LoginVO import LoginVO
 class IndustryVO(db.Model):
     __tablename__ = "industrymaster"
     Id = db.Column('Id', db.Integer, primary_key=True, autoincrement=True)
-    company_name = db.Column('company_name',db.String(50))
+    company_name = db.Column('company_name',db.String(70))
     designation = db.Column('designation',db.String(50))
-    work_description = db.Column('work_description',db.String(200))
-    start_year = db.Column('start_year', db.Integer)
-    end_year = db.Column('end_year', db.Integer)
+    work_description = db.Column('work_description',db.String(500))
+    no_of_months = db.Column('no_of_months', db.Integer)
     industry_loginId = db.Column('industry_loginId', db.Integer, db.ForeignKey(LoginVO.loginId))
 
     def as_dict(self):
@@ -17,8 +16,7 @@ class IndustryVO(db.Model):
             'company_name': self.company_name,
             'designation': self.designation,
             'work_description': self.work_description,
-            'start_year': self.start_year,
-            'end_date': self.end_year
+            'no_of_months': self.no_of_months
         }
 
 db.create_all()
