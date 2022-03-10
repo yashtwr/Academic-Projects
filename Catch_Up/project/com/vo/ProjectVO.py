742ff55543b8f049cd995f522d70c1beb53a7d19
Priyanka Jain <p52jain@uwaterloo.ca>
2022-03-11 02:02:58 +0530

    updated ProjectVO.py

--- Catch_Up/project/com/vo/ProjectVO.py
+++ Catch_Up/project/com/vo/ProjectVO.py
@@ -4,15 +4,15 @@ from project.com.vo.LoginVO import LoginVO
 class ProjectVO(db.Model):
     __tablename__ = "projectsmaster"
     Id = db.Column('Id', db.Integer, primary_key=True, autoincrement=True)
-    projects = db.Column('projects',db.String(20))
-    description = db.Column('description',db.String(200))
+    project_title = db.Column('project_title',db.String(100))
+    project_detail = db.Column('project_detail',db.String(500))
     project_loginId = db.Column('project_loginId', db.Integer, db.ForeignKey(LoginVO.loginId))
 
     def as_dict(self):
         return {
             'Id': self.Id,
-            'projects': self.projects,
-            'description': self.description
+            'project_title': self.project_title,
+            'project_detail': self.project_detail
         }
 
 db.create_all()
