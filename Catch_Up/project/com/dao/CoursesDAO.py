2fced840d16c6260544137d59e603347c7df9cf4
Jay Bharatkumar Soni <jb2soni@uwaterloo.ca>
2022-03-23 02:33:44 +0000

    Update CoursesDAO.py

--- Catch_Up/project/com/dao/CoursesDAO.py
+++ Catch_Up/project/com/dao/CoursesDAO.py
@@ -2,10 +2,17 @@ from project import db
 from project.com.vo.CoursesVO import CoursesVO
 
 class CoursesDAO:
+	# insert into database
     def insertCourses(self, coursesVO):
         db.session.add(coursesVO)
         db.session.commit()
 
+    # fetch records from database
     def fetchCourses(self, coursesVO):
         courses_list = CoursesVO.query.filter_by(course_loginId = coursesVO.course_loginId).all()
         return courses_list
+
+    # delete records from database
+    def deleteCourses(self, course_no, login_id):
+        CoursesVO.query.filter_by(course_no=course_no, course_loginId=login_id).delete()
+        db.session.commit()
