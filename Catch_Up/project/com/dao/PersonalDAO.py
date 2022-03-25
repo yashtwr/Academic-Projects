ede1124e4bb9836643773aa8766afe8c7b0999cb
Jay Bharatkumar Soni <jb2soni@uwaterloo.ca>
2022-03-25 05:37:55 +0000

    Update PersonalDAO.py update function added

--- Catch_Up/project/com/dao/PersonalDAO.py
+++ Catch_Up/project/com/dao/PersonalDAO.py
@@ -9,3 +9,11 @@ class PersonalDAO:
     def fetchPersonal(self, personalVO):
         personal_list = PersonalVO.query.filter_by(personal_loginId = personalVO.personal_loginId).all()
         return personal_list
+
+    def updatePersonal(self, personalVO):
+    	Personal_list = PersonalVO.query.filter_by(Id = personalVO.Id).first()
+    	Personal_list.description = personalVO.description
+    	Personal_list.contact_number = personalVO.contact_number
+    	Personal_list.contact_email = personalVO.contact_email
+    	Personal_list.address = personalVO.address
+    	db.session.commit()
