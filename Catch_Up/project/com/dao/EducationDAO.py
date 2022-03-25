b6f6eb819cadc194d187d154c9bc48ac7a42ebc9
Jay Bharatkumar Soni <jb2soni@uwaterloo.ca>
2022-03-25 23:58:27 +0000

    Update EducationDAO.py update method added

--- Catch_Up/project/com/dao/EducationDAO.py
+++ Catch_Up/project/com/dao/EducationDAO.py
@@ -9,3 +9,13 @@ class EducationDAO:
     def fetchEducation(self, educationVO):
         edu_list = EducationVO.query.filter_by(education_loginId = educationVO.education_loginId).all()
         return edu_list
+
+    def updateEducation(self, educationVO):
+        education_list = EducationVO.query.filter_by(Id = educationVO.Id).first()
+        education_list.degree_name = educationVO.degree_name
+        education_list.institution_name = educationVO.institution_name
+        education_list.start_date = educationVO.start_date
+        education_list.end_date = educationVO.end_date
+        education_list.cgpa = educationVO.cgpa
+        education_list.education_loginId = educationVO.education_loginId
+        db.session.commit()
