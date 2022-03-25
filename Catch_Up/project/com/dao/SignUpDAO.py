dbc1ad3c0969dea27816611332de7b1b7c86d14c
Jay Bharatkumar Soni <jb2soni@uwaterloo.ca>
2022-03-25 05:37:35 +0000

    Update SignUpDAO.py fetch and update function added

--- Catch_Up/project/com/dao/SignUpDAO.py
+++ Catch_Up/project/com/dao/SignUpDAO.py
@@ -6,3 +6,14 @@ class SignUpDAO:
     def insertUser(self,signupVO):
         db.session.add(signupVO)
         db.session.commit()
+
+    def fetchUser(self, signupVO):
+    	signup_list = SignUpVO.query.filter_by(signup_LoginId = signupVO.signup_LoginId).all()
+    	return signup_list
+
+
+    def updateUser(self, signupVO):
+    	signup_list = SignUpVO.query.filter_by(signup_LoginId = signupVO.signup_LoginId).first()
+    	signup_list.firstname = signupVO.firstname
+    	signup_list.lastname = signupVO.lastname
+    	db.session.commit()
