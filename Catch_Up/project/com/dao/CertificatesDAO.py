47c7179db2ad89a7ba53cd98979f73d1f97bfd01
Dhruv Jayesh Dholakia <djdholakia@uwaterloo.ca>
2022-03-25 04:31:49 +0000

    Update CertificatesDAO.py

--- Catch_Up/project/com/dao/CertificatesDAO.py
+++ Catch_Up/project/com/dao/CertificatesDAO.py
@@ -9,3 +9,11 @@ class CertificatesDAO:
     def fetchCertificates(self, certificatesVO):
         certificates_list = CertificatesVO.query.filter_by(certificates_loginId = certificatesVO.certificates_loginId).all()
         return certificates_list
+
+    def deleteCertificates(self, certificatesVO):
+        CertificatesVO.query.filter_by(Id=certificatesVO.Id, certificates_loginId=certificatesVO.certificates_loginId).delete()
+        db.session.commit()
+
+    def upadateCertificates(self, certificatesVO):
+        db.session.merge(certificatesVO)
+        db.session.commit()
