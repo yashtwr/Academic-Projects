184f6f77818ec99313a5d98ba7fd9f61396454f7
Jay Bharatkumar Soni <jb2soni@uwaterloo.ca>
2022-03-26 19:49:01 +0000

    Update ProjectDAO.py delete and update methods are done

--- Catch_Up/project/com/dao/ProjectDAO.py
+++ Catch_Up/project/com/dao/ProjectDAO.py
@@ -9,3 +9,15 @@ class ProjectDAO:
     def fetchProjects(self, projectVO):
         projects_list = ProjectVO.query.filter_by(project_loginId = projectVO.project_loginId).all()
         return projects_list
+
+    def deleteProject(self, projectVO):
+        ProjectVO.query.filter_by(Id=projectVO.Id).delete()
+        db.session.commit()
+
+    def updateProject(self, projectsVO):
+        # projects_list = ProjectsVO.query.filter_by(Id = projectsVO.Id).first()
+        # projects_list.project_title = projectsVO.project_title
+        # projects_list.project_detail = projectsVO.project_detail
+        # projects_list.project_loginId = projectsVO.project_loginId
+        db.session.merge(projectsVO)
+        db.session.commit()
