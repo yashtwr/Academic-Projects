a0e5af9ae42bf8f4598f2cb27a9b45612a7567bd
Dhruv Jayesh Dholakia <djdholakia@uwaterloo.ca>
2022-04-04 17:19:20 +0000

    Update SignUpDAO.py

--- Catch_Up/project/com/dao/SignUpDAO.py
+++ Catch_Up/project/com/dao/SignUpDAO.py
@@ -1,7 +1,7 @@
 from project import db
 
 from project.com.vo.SignUpVO import SignUpVO
-
+from sqlalchemy import func
 
 class SignUpDAO:
     def insertUser(self, signupVO):
@@ -20,6 +20,6 @@ class SignUpDAO:
 
     def fetchByName(self, signupVO):
         # signup_list = db.session.query(SignUpVO).filter(SignUpVO.firstname == signupVO.firstname)
-        signup_list = SignUpVO.query.filter_by(firstname=signupVO.firstname).all()
+        signup_list = db.session.query(SignUpVO).filter(func.lower(SignUpVO.firstname)==func.lower(signupVO.firstname)).all()
         print(signup_list)
         return signup_list
