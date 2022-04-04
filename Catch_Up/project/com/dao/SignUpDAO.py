9c90819b7dade885897f1638c16ce23c2b5d23ec
Dhruv Jayesh Dholakia <djdholakia@uwaterloo.ca>
2022-04-04 07:40:56 +0000

    Update SignUpDAO.py

--- Catch_Up/project/com/dao/SignUpDAO.py
+++ Catch_Up/project/com/dao/SignUpDAO.py
@@ -2,18 +2,24 @@ from project import db
 
 from project.com.vo.SignUpVO import SignUpVO
 
+
 class SignUpDAO:
-    def insertUser(self,signupVO):
+    def insertUser(self, signupVO):
         db.session.add(signupVO)
         db.session.commit()
 
     def fetchUser(self, signupVO):
-    	signup_list = SignUpVO.query.filter_by(signup_LoginId = signupVO.signup_LoginId).all()
-    	return signup_list
-
+        signup_list = SignUpVO.query.filter_by(signup_LoginId=signupVO.signup_LoginId).all()
+        return signup_list
 
     def updateUser(self, signupVO):
-    	signup_list = SignUpVO.query.filter_by(signup_LoginId = signupVO.signup_LoginId).first()
-    	signup_list.firstname = signupVO.firstname
-    	signup_list.lastname = signupVO.lastname
-    	db.session.commit()
+        signup_list = SignUpVO.query.filter_by(signup_LoginId=signupVO.signup_LoginId).first()
+        signup_list.firstname = signupVO.firstname
+        signup_list.lastname = signupVO.lastname
+        db.session.commit()
+
+    def fetchByName(self, signupVO):
+        # signup_list = db.session.query(SignUpVO).filter(SignUpVO.firstname == signupVO.firstname)
+        signup_list = SignUpVO.query.filter_by(firstname=signupVO.firstname).all()
+        print(signup_list)
+        return signup_list
